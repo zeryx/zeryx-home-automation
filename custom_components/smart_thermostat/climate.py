a2f1d9bde108fa450045420d48397b4094e11f65
zeryx <1892175+zeryx@users.noreply.github.com>
2025-01-14 18:43:42 -0500

    Implement Smart Thermostat Climate Entity with basic HVAC functionality and temperature control features

--- custom_components/smart_thermostat/climate.py
+++ custom_components/smart_thermostat/climate.py
@@ -1,5 +1,13 @@
 """Smart Thermostat Climate Platform"""
-from homeassistant.components.climate import ClimateEntity
+from homeassistant.components.climate import (
+    ClimateEntity,
+    HVACMode,
+    ClimateEntityFeature,
+)
+from homeassistant.const import (
+    ATTR_TEMPERATURE,
+    UnitOfTemperature,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
@@ -8,4 +16,59 @@ async def async_setup_platform(hass: HomeAssistant, config: ConfigType, async_ad
     async_add_entities([SmartThermostat(hass, config)])
 
 class SmartThermostat(ClimateEntity):
-    # ... existing SmartThermostatControl code adapted to extend ClimateEntity ... 
+    """Smart Thermostat Climate Entity."""
+    
+    def __init__(self, hass, config):
+        """Initialize the thermostat."""
+        self._hass = hass
+        self._name = config.get("name", "Smart Thermostat")
+        self._hvac_mode = HVACMode.OFF
+        self._target_temperature = 20
+        self._current_temperature = None
+        self._unit = UnitOfTemperature.CELSIUS
+
+    @property
+    def name(self):
+        """Return the name of the thermostat."""
+        return self._name
+
+    @property
+    def temperature_unit(self):
+        """Return the unit of measurement."""
+        return self._unit
+
+    @property
+    def current_temperature(self):
+        """Return the current temperature."""
+        return self._current_temperature
+
+    @property
+    def target_temperature(self):
+        """Return the temperature we try to reach."""
+        return self._target_temperature
+
+    @property
+    def hvac_mode(self):
+        """Return current operation."""
+        return self._hvac_mode
+
+    @property
+    def hvac_modes(self):
+        """Return the list of available operation modes."""
+        return [HVACMode.OFF, HVACMode.HEAT, HVACMode.COOL]
+
+    @property
+    def supported_features(self):
+        """Return the list of supported features."""
+        return ClimateEntityFeature.TARGET_TEMPERATURE
+
+    async def async_set_temperature(self, **kwargs):
+        """Set new target temperature."""
+        if (temp := kwargs.get(ATTR_TEMPERATURE)) is not None:
+            self._target_temperature = temp
+            self.async_write_ha_state()
+
+    async def async_set_hvac_mode(self, hvac_mode):
+        """Set new target operation mode."""
+        self._hvac_mode = hvac_mode
+        self.async_write_ha_state() 
